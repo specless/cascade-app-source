80369b0fcebb4edaa2695e763722e30d804397af
Stephen Corby <steve@gospecless.com>
2016-07-22 10:36:48 -0500

    o

---
+++ compiler/templates/component.js
@@ -0,0 +1,39 @@
+{{{license}}}
+window.Specless.userJS(window, function (specless, _, extendFrom, factories, ad, $, plugins) {
+    (function (requiresList, prerequisites, userJs) {
+        var _ = window.Specless.scope()._,
+            counter = 0,
+            checkFinished = function () {
+                counter--;
+                if (counter) {
+                    return;
+                }
+                userJs();
+            };
+        if (requiresList.length && requiresList[0]) {
+            counter++;
+            ad.requires(requiresList, checkFinished);
+        }
+        _.each(prerequisites, function (should, key) {
+            if (!should) {
+                return;
+            }
+            counter++;
+            ad['activate' + _.upCase(key)](checkFinished);
+        });
+        counter++;
+        checkFinished();
+    }(
+        // don't touch anything above this line
+        [{{{cdnscripts}}}], {
+            device: true,
+            location: true,
+            time: true
+        }, function () {
+            {{{pluginfragments}}}
+			//================ Begin User Created JS ==================
+			{{{userjs}}}
+			//================  End User Created JS  ==================
+        }));
+});
+
